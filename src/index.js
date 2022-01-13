f92a360b98a0f81f04dc4bc4456b6bc522cc0421
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:47:56 -0500

    remove old index.js code and type:module from package.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import { pipe } from 'lodash/fp';
+import store from './store';
 
-let input = "    JavaScript  ";
-let output = "<div>" + input.trim() + "</div>";
-
-const trim = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`;
-const toLowerCase = str => str.toLowerCase();
-
-// compose is a higher order function. Here we are just passing references to them, not calling them
-const transform = pipe(trim, toLowerCase, wrapInDiv);
-// now we don't have to nest the functions from "result"
-// console.log("transform", transform(input));
-
-//must read right to left. And too much parathesis.
-// const result = wrapInDiv(toLowerCase(trim(input)));
-// console.log("result", result);
+console.log("store", store);
