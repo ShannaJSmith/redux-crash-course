e778e726504f82ab15d5375f728a2ede6de82dea
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:42:02 -0500

    complete reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,24 @@
-// []
+// [] <- Store (initial state is the empty array)
 let lastId = 0;
 
-function reducer(state, action) {
+// purpose of reducer is to return new state based on the action:
+function reducer(state = [], action) {
   if (action.type === 'ADD_BUG')
-  return [
-    ...state,
-    {
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false
-    }
-  ];
+    // return new state
+    return [
+      ...state, // copy bugs in current state
+      // then add new bug object
+      {
+        id: ++lastId, // every time we add a bug we will increment the id
+        description: action.payload.description,
+        resolved: false,
+      },
+    ];
   else if (action.type === 'REMOVE_BUG')
-    return state.filter(bug => bug.id !== action.payload.id)
-    return state;
+    // return a new array with all the bugs except the one with the given id
+    return state.filter((bug) => bug.id !== action.payload.id)
+  // return current state if the above action doesn't exist
+  return state;
 }
+
+// this reducer is a pure function
