1d238fbc4f11d6d271841b50f89c74c5ad64f256
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 22:04:06 -0500

    install lodash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { compose, pipe } from "lodash/fp";
 let input = "    JavaScript  ";
 let output = "<div>" + input.trim() + "</div>";
 
@@ -5,5 +6,6 @@ const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`;
 const toLowerCase = str => str.toLowerCase();
 
+//must read right to left. And too much parathesis.
 const result = wrapInDiv(toLowerCase(trim(input)))
 console.log("result", result)
