feeb0c00a814a2c081496300b618f8c68f746c69
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:52:19 -0500

    implement actions on store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import store from './store';
 
-console.log("store", store);
+//dispatch action for adding bug
+store.dispatch({
+  type: "ADD_BUG",
+  payload: {
+    description: "Bug1"
+  }
+});
+
+//dispatch action for removing bug
+store.dispatch({
+  type: "REMOVE_BUG",
+  payload: {
+    id: 1
+  }
+})
+
+console.log("store", store.getState());
