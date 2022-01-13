3ead29c03298980e6860b500597d4d8cf59eb6e5
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:17:05 -0500

    update notes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const toLowerCase = str => str.toLowerCase();
 // compose is a higher order function. Here we are just passing references to them, not calling them
 const transform = pipe(trim, toLowerCase, wrapInDiv);
 // now we don't have to nest the functions from "result"
-console.log("transform", transform(input));
+// console.log("transform", transform(input));
 
 //must read right to left. And too much parathesis.
 // const result = wrapInDiv(toLowerCase(trim(input)));
