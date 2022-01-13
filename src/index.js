0d9c21fad01a2cc1df406f137ee00cc2e62edeb4
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:59:34 -0500

    implement subscription and unsubscribe actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import store from './store';
 
+// function gets called everytime the state of store changes
+// UI should subscribed so they get notified when the state of store changes
+store.subscribe(() => {
+  console.log("Store changed~", store.getState());
+})
+
+// user may nagivate away from page. new page doesn't have ui componenet so subscription not needed
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed~", store.getState());
+// })
+
 //dispatch action for adding bug
 store.dispatch({
   type: "ADD_BUG",
@@ -7,6 +18,8 @@ store.dispatch({
     description: "Bug1"
   }
 });
+
+// unsubscribe(); <- only one msg will log now
 
 //dispatch action for removing bug
 store.dispatch({
