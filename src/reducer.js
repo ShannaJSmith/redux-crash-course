e07acadbc20507808717a543014f86e025f247af
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-12 23:33:51 -0500

    update notes, implement reducer

---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+// []
+let lastId = 0;
+
+function reducer(state, action) {
+  if (action.type === 'ADD_BUG')
+  return [
+    ...state,
+    {
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false
+    }
+  ];
+  else if (action.type === 'REMOVE_BUG')
+    return state.filter(bug => bug.id !== action.payload.id)
+    return state;
+}
